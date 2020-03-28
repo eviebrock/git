a8c3763580396e6022314eae59de522d90faff73
Erich Viebrock <erich.viebrock@gmail.com>
2020-03-28 13:09:04 -0400

    Updating contact section and work experience

--- personal/website/bootstrap/php/contact.php
+++ personal/website/bootstrap/php/contact.php
@@ -1,27 +1,25 @@
 <?php
+$emailTo = 'erich.viebrock@gmail.com';
+//$toName = 'Erich Viebrock';
+$subject = 'Message from erichviebrock.com Portfolio';
+
 if(isset( $_POST['name']))
-	$name = $_POST['name'];
+	$fromName = $_POST['name'];
 if(isset( $_POST['email']))
-	$email = $_POST['email'];
+	$emailFrom = $_POST['email'];
 if(isset( $_POST['message']))
 	$message = $_POST['message'];
-
-$emailto = 'erich.viebrock@gmail.com';
-$toname = 'Erich Viebrock';
-$emailfrom = $email;
-$fromname = $name;
-$subject = 'Message from erichviebrock.com Portfolio';
-$messagebody = $message;
+	
 $headers = 
-	'Return-Path: ' . $emailfrom . "\r\n" . 
-	'From: ' . $fromname . ' <' . $emailfrom . '>' . "\r\n" . 
+	'Return-Path: ' . $emailFrom . "\r\n" . 
+	'From: ' . $fromName . ' <' . $emailFrom . '>' . "\r\n" . 
 	'X-Priority: 3' . "\r\n" . 
 	'X-Mailer: PHP ' . phpversion() .  "\r\n" . 
-	'Reply-To: ' . $fromname . ' <' . $emailfrom . '>' . "\r\n" .
+	'Reply-To: ' . $fromName . ' <' . $emailFrom . '>' . "\r\n" .
 	'MIME-Version: 1.0' . "\r\n" . 
 	'Content-Transfer-Encoding: 8bit' . "\r\n" . 
 	'Content-Type: text/plain; charset=UTF-8' . "\r\n";
-$params = '-f ' . $emailfrom;
-$status = mail($emailto, $subject, $messagebody, $headers, $params);
+$params = '-f ' . $emailFrom;
+$status = mail($emailTo, $subject, $message, $headers, $params);
 echo $status
 ?>
